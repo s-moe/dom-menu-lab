5b65ed84d985fa5ffc3918f784e17e451d3891f7
Sarah Moe <sarahmoe@Sarahs-MacBook-Pro.local>
2021-05-08 10:58:26 -0700

    made changes

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//Task 1.0
+const mailEl = document.querySelector('main');
+//Task 1.1.
+mailEl.style.backgroundColor = 'var(--main-bg)';
+//Task 1.2
+mainEl.innerHTML = '<h1>SEI Rocks!</h1>'
+//how do we check to see if it's working? 
