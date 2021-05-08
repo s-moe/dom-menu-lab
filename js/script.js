2d30ddcd4e195971980733315d97adbc913382b7
Sarah Moe <sarahmoe@Sarahs-MacBook-Pro.local>
2021-05-08 11:23:04 -0700

    made changes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 //Task 1.0
-const mailEl = document.querySelector('main');
+const mainEl = document.querySelector('main');
 //Task 1.1.
-mailEl.style.backgroundColor = 'var(--main-bg)';
+mainEl.style.backgroundColor = 'var(--main-bg)';
 //Task 1.2
-mainEl.innerHTML = '<h1>SEI Rocks!</h1>'
-//how do we check to see if it's working? 
+mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
+//how do we check to see if it's working?
+
+//Task 1.3
+mainEl.classList.add('flex-ctr');
